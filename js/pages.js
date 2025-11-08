83a2fd2dc014fca9d238a5a51efcc2171325b8a0
Ksquiitoox <ing.alejandroguerreno@gmail.com>
2025-11-08 04:21:30 -0300

    dddddddddddd

---
+++ js/pages.js
@@ -0,0 +1,9 @@
+const pages = [
+  { title: "Home", url: "index.html" },
+  { title: "Categorías", url: "Categorias.html" },
+  { title: "Laptops", url: "Laptops.html" },
+  { title: "Smartphones", url: "Smartphones.html" },
+  { title: "Accesorios", url: "Accesorios.html" },
+  { title: "Login", url: "login.html" },
+  { title: "Registro", url: "register.html" }
+];
