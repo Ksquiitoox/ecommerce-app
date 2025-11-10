f071007a1bb5c95daa2960b7e9e61be92a767c32
Ksquiitoox <ing.alejandroguerreno@gmail.com>
2025-11-10 03:09:12 -0300

    no che

--- js/register.js
+++ js/register.js
@@ -21,7 +21,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const nuevoUsuario = { nombre, apellido, email, password, fecha };
     usuarios.push(nuevoUsuario);
     localStorage.setItem("usuarios", JSON.stringify(usuarios));
-    localStorage.setItem("usuarioLogueado", JSON.stringify(usuario));
     alert(`Registro exitoso. ¡Bienvenido ${nombre}!`);
     window.location.href = "index.html";
   });
