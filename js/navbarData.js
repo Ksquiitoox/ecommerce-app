e21ddd148401282504eb7b711fba0225bd9626cf
Ksquiitoox <ing.alejandroguerreno@gmail.com>
2025-11-10 02:36:56 -0300

    lunes 3am

--- js/navbarData.js
+++ js/navbarData.js
@@ -1,10 +1,10 @@
 const navbarItems = [
-  { title: "Home", url: "index.html" },
+  { title: "Home", url: "Index.html" },
   { title: "Categorías", url: "Categorias.html" },
   { title: "Laptops", url: "Laptops.html" },
   { title: "Smartphones", url: "Smartphones.html" },
   { title: "Accesorios", url: "Accesorios.html" },
-  { title: "Login", url: "login.html" },
-  { title: "Registro", url: "register.html" },
+  { title: "Login", url: "Login.html" },
+  { title: "Registro", url: "Register.html" },
   { title: "Logout", url: "#", id: "logoutBtn" }
 ];
